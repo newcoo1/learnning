442b3e6be1412cffb74bc26cb3c06f313fd561c3
newcool <newcool@qq.com>
2017-10-26 08:04:23 +0800

    backup

--- c/wks/main.c
+++ c/wks/main.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <ctype.h>
 
-int n=1;
-int f1(){
-  return ++n;
-}
-int f2(){
-  return n*n;
+int binsearch(int x,int v[],int len){
+  int low=0;
+  int high=len-1;
+  int mid=(low+high)/2;
+  while( (low <=high)&&(v[mid]!=x) ){
+    printf("%d %d \n",low,high);
+    (x<v[mid])&&(high=mid-1);
+    (x>v[mid])&&(low=mid+1);
+    mid=(low+high)/2;
+  }
+  //(x==v[mid]) && (return mid);
+  return (x==v[mid]) ? mid:-1;
 }
 int main(void){
-  printf("%d %d",f2(),f1() );
+  int numbers[]={0,1,2,3,4,5};
+  printf("%d \n",binsearch(-1,numbers,6) );//sizeof(numbers)/sizeof(int )));  
   return 0;
 }
-
-
